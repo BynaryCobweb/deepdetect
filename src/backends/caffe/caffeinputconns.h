1ca4a634de23eb05e07f6909f47473302de62f6e
Emmanuel Benazera <beniz@droidnik.fr>
2019-04-23 15:53:49 +0200

    added USE_LIBVNN option at build time to use libvnn for video input connector with GPU support

--- src/backends/caffe/caffeinputconns.h
+++ src/backends/caffe/caffeinputconns.h
@@ -27,7 +27,9 @@
 #include "csvtsinputfileconn.h"
 #include "txtinputfileconn.h"
 #include "svminputfileconn.h"
+#ifdef USE_LIBVNN
 #include "vidinputconn.h"
+#endif
 #include "caffe/caffe.hpp"
 #include "caffe/util/db.hpp"
 #include "utils/fileops.hpp"
@@ -534,6 +536,7 @@ namespace dd
     bool _align = false;
   };
 
+#ifdef USE_LIBVNN
   /**
    * \brief Video image connector, supports both files and camera stream
    */
@@ -718,7 +721,8 @@ namespace dd
     int _dt_seg = 0;
     bool _align = false;
   };
-
+#endif
+  
   /**
    * \brief Caffe CSV connector
    * \note use 'label_offset' in API to make sure that labels start at 0
