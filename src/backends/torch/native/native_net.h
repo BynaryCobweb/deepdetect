e5fc6afaa6b78e2c6106c5665e58c8545edc3fef
Louis Jean <lsj456@gmail.com>
2020-11-27 12:24:41 +0100

    feat(torch): replace pytorch cloneable with custom DDCloneable

--- src/backends/torch/native/native_net.h
+++ src/backends/torch/native/native_net.h
@@ -29,6 +29,10 @@
 
 #include "../torchinputconns.h"
 
+using namespace torch;
+using namespace torch::nn;
+using namespace c10;
+
 namespace dd
 {
 
@@ -75,8 +79,95 @@ namespace dd
     virtual void update_input_connector(TorchInputInterface &inputc) = 0;
   };
 
+  template <typename Derived>
+  class DDCloneable : public virtual torch::nn::Module
+  {
+  public:
+    virtual ~DDCloneable() = default;
+    using Module::Module;
+
+    /// `reset()` must perform initialization of all members with reference
+    /// semantics, most importantly parameters, buffers and submodules.
+    virtual void reset() = 0;
+
+    /// Performs a recursive "deep copy" of the `Module`, such that all
+    /// parameters and submodules in the cloned module are different from those
+    /// in the original module.
+    std::shared_ptr<Module> clone(const optional<Device> &device
+                                  = nullopt) const override
+    {
+      NoGradGuard no_grad;
+
+      const auto &self = static_cast<const Derived &>(*this);
+      auto copy = std::make_shared<Derived>(self);
+      copy->parameters_.clear();
+      copy->buffers_.clear();
+      copy->children_.clear();
+      copy->reset();
+      // [[this pointer note]]
+      // Don't remove 'this' pointer, nvcc needs it to be explicitly given in
+      // some envs. eg. ubuntu 16.04 + gcc 5.x + cuda 9.2
+      //     ubuntu 16.04 + gcc 7.x + cuda 9.2
+      TORCH_CHECK(
+          copy->parameters_.size() == this->parameters_.size(),
+          "The cloned module does not have the same number of "
+          "parameters as the original module after calling reset(). "
+          "Are you sure you called register_parameter() inside reset() "
+          "and not the constructor?");
+      for (const auto &parameter : named_parameters(/*recurse=*/false))
+        {
+          auto &tensor = *parameter;
+          auto data = device && tensor.device() != *device
+                          ? tensor.to(*device)
+                          : autograd::Variable(tensor).clone();
+          copy->parameters_[parameter.key()].set_data(data);
+        }
+      // Don't remove 'this' pointer. See [[this pointer note]]
+      TORCH_CHECK(copy->buffers_.size() == this->buffers_.size(),
+                  "The cloned module does not have the same number of "
+                  "buffers as the original module after calling reset(). "
+                  "Are you sure you called register_buffer() inside reset() "
+                  "and not the constructor?");
+      for (const auto &buffer : named_buffers(/*recurse=*/false))
+        {
+          auto &tensor = *buffer;
+          auto data = device && tensor.device() != *device
+                          ? tensor.to(*device)
+                          : autograd::Variable(tensor).clone();
+          copy->buffers_[buffer.key()].set_data(data);
+        }
+      // Don't remove 'this' pointer. See [[this pointer note]]
+      TORCH_CHECK(
+          copy->children_.size() == this->children_.size(),
+          "The cloned module does not have the same number of "
+          "child modules as the original module after calling reset(). "
+          "Are you sure you called register_module() inside reset() "
+          "and not the constructor?");
+      // Don't remove 'this' pointer. See [[this pointer note]]
+      for (const auto &child : this->children_)
+        {
+          copy->children_[child.key()]->clone_(*child.value(), device);
+        }
+      return copy;
+    }
+
+  private:
+    void clone_(Module &other, const optional<Device> &device) final
+    {
+      // Here we are *pretty* certain that `other's` type is `Derived` (because
+      // it was registered under the same name as `this`), but you never know
+      // what crazy things `reset()` does, so `dynamic_cast` just to be safe.
+      auto clone = std::dynamic_pointer_cast<Derived>(other.clone(device));
+      TORCH_CHECK(
+          clone != nullptr,
+          "Attempted to clone submodule, but it is of a "
+          "different type than the submodule it was to be cloned into");
+      static_cast<Derived &>(*this) = std::move(*clone);
+    }
+  };
+
   template <typename T>
-  class NativeModuleImpl : public NativeModule, public torch::nn::Cloneable<T>
+  class NativeModuleImpl : public NativeModule, public DDCloneable<T>
   {
   };
 }
